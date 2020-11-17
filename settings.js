c58dcaff21f0684d1859ba1742383c94b150b5e9
Tuhin Kanti Pal <me@mailtuhin.ml>
2020-11-17 15:48:11 +0530

    Migrated

---
+++ settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Enter your Hoichoi Credentials
+
+    email: "you@email.com",
+    password: "nubpassword",
+
+    // Enter here any random alphabet string
+    randomString: "abcDgfHoicjsoeij"
+}
